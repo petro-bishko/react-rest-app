30d8770e1b9ffd03020cb06e1f4ceb5f0cedd043
petro.bishko <petro.bishko.uk@gmail.com>
2019-10-29 14:14:44 +0200

    Updates

---
+++ precache-manifest.8c6baa30a6d0ea0e24fd75505f83238e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb806c0e0bf0e1dc7f6152b9495fb5f9",
+    "url": "/react-rest-app/index.html"
+  },
+  {
+    "revision": "7f8b7a7ac6de3f59d6ee",
+    "url": "/react-rest-app/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "3703f0cd956f7c23bfe9",
+    "url": "/react-rest-app/static/css/main.7071cfab.chunk.css"
+  },
+  {
+    "revision": "7f8b7a7ac6de3f59d6ee",
+    "url": "/react-rest-app/static/js/2.b4fdfd90.chunk.js"
+  },
+  {
+    "revision": "3703f0cd956f7c23bfe9",
+    "url": "/react-rest-app/static/js/main.00399332.chunk.js"
+  },
+  {
+    "revision": "b02d5628d947e6407c7b",
+    "url": "/react-rest-app/static/js/runtime-main.c96ff51b.js"
+  }
+]);
