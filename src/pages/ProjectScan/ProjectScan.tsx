935353692c4906502ca62d8b15de39f880bed97f
petro.bishko <petro.bishko.uk@gmail.com>
2019-10-30 18:59:24 +0200

    aws-test

--- src/pages/ProjectScan/ProjectScan.tsx
+++ src/pages/ProjectScan/ProjectScan.tsx
@@ -1,24 +1,43 @@
-import React, { useEffect, useState } from 'react';
+import React, { useRef, useState } from 'react';
 import axios from 'axios';
 import './ProjectScan.css'
+import Loading from './loading.gif';
 
 const ProjectScan: React.FC = () => {
-  const [project, setProject] = useState({});
-  const createProject = () => {
-    axios.post('http://localhost:1000/v1/projects', {
-      name: 'Test project',
-      folders: 2,
-      files: 3
-    }).then((res) => setProject(res.data))
+
+  const [file, setFile] = useState();
+  const [loader, setLoader] = useState(false);
+  const [projectScanData, setProjectScanData] = useState([]);
+  const [error, setError] = useState();
+  const inputEl = useRef<HTMLInputElement>(null);
+
+  const scanProject = () => {
+    const data = new FormData();
+    data.append('file', file);
+
+    setLoader(true);
+    setError(null);
+
+    axios.post("http://localhost:1000/v1/scan-project", data).then((res) => res.data)
+      .then((res) => res.data)
+      .then(value => {
+        setLoader(false);
+        setProjectScanData(value.filesName);
+      }).catch((err) => {
+      setError('Something wrong');
+      setLoader(false);
+    }).finally(() => {
+      if (inputEl.current) {
+        inputEl.current.value = "";
+      }
+
+    });
   };
 
-  useEffect(() => {
-    axios.get('http://localhost:1000/v1/projects').then((res) => res.data)
-      .then(value => {
-        console.log(value); // TODO remove console.log
-        setProject(value);
-      })
-  }, []);
+  const onUploadFile = (event: any) => {
+    setFile(event.target.files[0]);
+  };
+
   return (
     <React.Fragment>
       <section className="jumbotron text-center">
@@ -28,13 +47,16 @@ const ProjectScan: React.FC = () => {
             Check your project for vulnerabilities
           </p>
           <p>
-            <button onClick={() => createProject()} className="btn btn-dark">Upload Project</button>
+            <input ref={inputEl} type="file" name="file" onChange={onUploadFile}/>
+            <button onClick={() => scanProject()} className="btn btn-dark">Scan Project</button>
           </p>
+          <p>{loader ? <img alt={Loading} src={Loading}/> : ''}</p>
+          {error ? <p style={{color: 'tomato'}}>ERROR: {error}</p> : null}
         </div>
       </section>
       <section>
-        <div className="container">
-          <h3>Project name</h3>
+        {projectScanData.length ? <div className="container">
+          <h3>Scan data</h3>
           <table className="table table-striped table-dark">
             <thead>
             <tr>
@@ -46,16 +68,22 @@ const ProjectScan: React.FC = () => {
             </tr>
             </thead>
             <tbody>
-            <tr>
-              <th scope="row">1</th>
-              <td>2</td>
-              <td>23</td>
-              <td>1200</td>
-              <td><a href="/">File name</a></td>
-            </tr>
+            {projectScanData.map((value, index) => {
+              return (
+                <tr key={index}>
+                  <th scope="row">1</th>
+                  <td>2</td>
+                  <td>23</td>
+                  <td>1200</td>
+                  <td><a href="/">{value}</a></td>
+                </tr>
+              )
+            })}
+
             </tbody>
           </table>
-        </div>
+        </div> : null}
+
       </section>
     </React.Fragment>
   )
